65fe613260bf0c190a145952b071d2b159dcfaef
jkosmicki2024 <jkosmicki@unomaha.edu>
2025-02-05 16:04:42 +0000

    Done with Lab

--- RPS.py
+++ RPS.py
@@ -10,16 +10,56 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  import random
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+import random
 
-  #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
+def main():
+    playagain = "Y"
+    wins = 0
+    ties = 0
+    losses = 0
+
+    while playagain == "Y":
+        computer = random.choice(["R", "P", "S"])
+        player = input("Choose one (R, P, S): ").upper()
+
+        if computer == "R":
+            print("Computer picked rock")
+        elif computer == "P":
+            print("Computer picked paper")
+        elif computer == "S":
+            print("Computer picked scissors")
+
+        if player == computer:
+            print("Tie")
+            ties += 1
+        elif player == "R" and computer == "S":
+            print("You win")
+            wins += 1
+        elif player == "R" and computer == "P":
+            print("You lose")
+            losses += 1
+        elif player == "P" and computer == "R":
+            print("You win")
+            wins += 1
+        elif player == "P" and computer == "S":
+            print("You lose")
+            losses += 1
+        elif player == "S" and computer == "P":
+            print("You win")
+            wins += 1
+        elif player == "S" and computer == "R":
+            print("You lose")
+            losses += 1
+
+        playagain = input("Play again? (Y/N): ").upper()
+
+    # Print the stats
+    print("Wins \t Ties \t Losses")
+    print("---- \t ---- \t ------")
+    print(wins, "\t", ties, "\t", losses)
 
 if __name__ == '__main__':
-  main()
+    main()
+
